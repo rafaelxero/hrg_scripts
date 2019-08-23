1a8e883c8a5a6714b3a56704fb4d6c80d66b86cc
Rafael Cisneros <rafael.cisneros@aist.go.jp>
2019-08-23 17:20:19 +0900

    Used simdate for the folder

--- drcframework/plot_joint_motion.m
+++ drcframework/plot_joint_motion.m
@@ -1,10 +1,10 @@
 function plot_joint_motion(j)
 
-%folder = "~/tmp/log/20190821/";
-folder = "/tmp/";
-
 simdate = "20190823";
-simtime = "0357"; % 1339
+simtime = "1517";  # 1524 1518 1517
+
+folder = ["~/tmp/log/", simdate, "/"];
+%folder = "/tmp/";
 
 joint_motion_log = get_structure([folder, "hmc_", simdate, simtime, "-joint-motion.log"]);
 
