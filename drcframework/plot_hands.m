83d6f913b41418be2e89a97bcf71c596dd986d9e
Rafael Cisneros <rafael.cisneros@aist.go.jp>
2019-08-29 18:09:31 +0900

    Added plot_hands

---
+++ drcframework/plot_hands.m
@@ -0,0 +1,51 @@
+function plot_hands(simdate, simtime)
+
+folder = "/tmp/";
+
+car_comp = ["x", "y", "z"];
+rot_comp = {"Rpy", "rPy", "rpY"};
+
+task_space_motion_log = get_structure([folder, "hmc_", simdate, simtime, "-task-space-motion.log"]);
+
+t = task_space_motion_log.time;
+
+for i = 1:3
+  RHxyz_des(:, i) = task_space_motion_log.(["rhand-des_P", car_comp(i)]);
+  RHxyz_hat(:, i) = task_space_motion_log.(["rhand-hat_P", car_comp(i)]);
+end
+
+for i = 1:3
+  RHrpy_des(:, i) = task_space_motion_log.(["body-des_R_", rot_comp{i}]);
+  RHrpy_hat(:, i) = task_space_motion_log.(["body-hat_R_", rot_comp{i}]);
+end
+
+figure(1)
+clf
+
+for j = 1:3
+
+  subplot(3, 2, 2*j - 1)
+
+  title(["RH", car_comp(j), " des vs hat"], 'fontsize', 30)
+  hold on;
+  grid on;
+
+  plot(t, RHxyz_des(:, j),  'Color', 'magenta',   'LineWidth', 2);
+  plot(t, RHxyz_hat(:, j),  'Color', 'red', 'LineWidth', 2);
+
+  h = get(gcf, "currentaxes");
+  set(h, "fontsize", 30);
+  
+  subplot(3, 2, 2*j)
+
+  title(["RH", rot_comp{j}, " des vs hat"], 'fontsize', 30)
+  hold on;
+  grid on;
+
+  plot(t, RHrpy_des(:, j),  'Color', 'magenta',   'LineWidth', 2);
+  plot(t, RHrpy_hat(:, j),  'Color', 'red', 'LineWidth', 2);
+
+  h = get(gcf, "currentaxes");
+  set(h, "fontsize", 30);
+  
+end
